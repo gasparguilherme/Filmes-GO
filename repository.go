6ab2ffa1b44da5397244c08f7fc668a18b71f0c3
Guilherme Gaspar <guilhermegaspar474@gmail.com>
2024-07-18 16:50:21 -0300

    criando estruturas usuario

--- repository.go
+++ repository.go
@@ -1,3 +1,32 @@
 package main  
+import "fmt"
 
-func main() {}
+var idAtual = 1
+
+type Usuario struct{
+	Nome string
+	Email string
+	ID int
+}
+
+func novoUsuario(nome, email string)Usuario{
+	usuario := Usuario{
+		Nome: nome,
+		Email: email,
+		ID: idAtual,
+	}
+	idAtual++
+	return usuario
+}
+
+
+func main() {
+
+	guilherme := novoUsuario("Guilherme", "guilhermegaspar@gmail.com")
+	fmt.Println(guilherme)
+
+	joao := novoUsuario("Joao", "joaosaraceni@gmail,com")
+	fmt.Println(joao)
+
+
+}
