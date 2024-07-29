b906976d27b5a2aa2e8bd7ac34c67d5df23b6626
Guilherme Gaspar <guilhermegaspar474@gmail.com>
2024-07-29 13:07:06 -0300

    Criação da entidade Filme, criação de uma Slice Global para o armazenamento de usuario

--- repository.go
+++ repository.go
@@ -2,11 +2,21 @@ package main
 import "fmt"
 
 var idAtual = 1
+var listaUsuarios []Usuario
+
 
 type Usuario struct{
 	Nome string
 	Email string
 	ID int
+}
+
+type Filme struct{
+	Titulo string
+	Diretor string
+	Ano int
+	Genero string
+
 }
 
 func novoUsuario(nome, email string)Usuario{
@@ -16,7 +26,9 @@ func novoUsuario(nome, email string)Usuario{
 		ID: idAtual,
 	}
 	idAtual++
+	listaUsuarios = append(listaUsuarios,usuario )
 	return usuario
+
 }
 
 
@@ -24,9 +36,11 @@ func main() {
 
 	guilherme := novoUsuario("Guilherme", "guilhermegaspar@gmail.com")
 	fmt.Println(guilherme)
-
+	
 	joao := novoUsuario("Joao", "joaosaraceni@gmail.com")
 	fmt.Println(joao)
+
+	fmt.Println(listaUsuarios)
 
 
 }
