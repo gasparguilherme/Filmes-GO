58f0131e9e8424a6326cbd943a1e87b2308db6f9
Guilherme Gaspar <guilhermegaspar474@gmail.com>
2025-02-06 16:14:26 -0300

    melhorias na sintaxe das entidades

--- domain/entities/users.go
+++ domain/entities/users.go
@@ -1,7 +1,7 @@
 package entities
 
 type User struct {
-	Name  string `json: "name"`
-	Email string `json: "email"`
-	ID    int    `json: "id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
+	ID    int    `json:"id"`
 }
