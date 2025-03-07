ea515376bac6895a3f4262ede35fd8e5d8f6aca2
Guilherme Gaspar <guilhermegaspar474@gmail.com>
2025-03-07 14:54:33 -0300

    transformando os handlers em handlers HTTP

--- handlers/film/films_by_id.go
+++ handlers/film/films_by_id.go
@@ -3,16 +3,16 @@ package film
 import (
 	"encoding/json"
 	"log/slog"
+	"net/http"
 
 	"github.com/gasparguilherme/my-repository/domain/entities"
 	"github.com/gasparguilherme/my-repository/domain/usecases"
 	"github.com/gasparguilherme/my-repository/handlers/validate"
 )
 
-func HandleGetFilmByID(jsonInput []byte) {
-	slog.Info("requisição de busca de filme por ID", "JSON", string(jsonInput))
+func HandleGetFilmByID(w http.ResponseWriter, r *http.Request) {
 	var f entities.Film
-	err := json.Unmarshal(jsonInput, &f)
+	err := json.NewDecoder(r.Body).Decode(&f)
 	if err != nil {
 		slog.Error("não foi possivel interpretar json", "error", err)
 		return
@@ -26,15 +26,14 @@ func HandleGetFilmByID(jsonInput []byte) {
 
 	f, err = usecases.FilmByID(f.ID)
 	if err != nil {
-		slog.Error("falha ao buscar filme pelo id", "error", err)
+		slog.Error("falha ao buscar filme", "error", err)
 		return
 	}
 
-	jsonFilm, err := json.Marshal(f)
+	err = json.NewEncoder(w).Encode(f)
 	if err != nil {
 		slog.Error("erro ao converter para formato JSON", "error", err)
 		return
 	}
 
-	slog.Info("filme encontrado", "filme", string(jsonFilm))
 }
--- handlers/user/users.go
+++ handlers/user/users.go
@@ -3,16 +3,16 @@ package user
 import (
 	"encoding/json"
 	"log/slog"
+	"net/http"
 
 	"github.com/gasparguilherme/my-repository/domain/entities"
 	"github.com/gasparguilherme/my-repository/domain/usecases"
 	"github.com/gasparguilherme/my-repository/handlers/validate"
 )
 
-func CreateUser(jsonInput []byte) {
-	slog.Info("requisição de criação de usuario", "JSON", string(jsonInput))
+func CreateUser(w http.ResponseWriter, r *http.Request) {
 	var user *entities.User
-	err := json.Unmarshal(jsonInput, &user)
+	err := json.NewDecoder(r.Body).Decode(&user)
 	if err != nil {
 		slog.Error("não foi possivel interpretar o JSON", "error", err)
 		return
@@ -26,12 +26,10 @@ func CreateUser(jsonInput []byte) {
 
 	user = usecases.NewUser(user.Name, user.Email)
 
-	jsonUser, err := json.Marshal(user)
+	err = json.NewEncoder(w).Encode(user)
 	if err != nil {
 		slog.Error("erro ao converter para formato JSON", "error", err)
 		return
 	}
-
-	slog.Info("usuario criado com sucesso", "usuario", string(jsonUser))
 
 }
