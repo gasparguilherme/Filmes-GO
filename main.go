7c341b0729b31e6fcbadf0176cbf490df565b691
Guilherme Gaspar <guilhermegaspar474@gmail.com>
2025-07-01 11:51:57 -0300

    cria uma conexao com o banco usando a funcao connect da lib pgx

--- main.go
+++ main.go
@@ -1,12 +1,31 @@
 package main
 
 import (
+	"context"
+	"log/slog"
+	"os"
+
 	"github.com/gasparguilherme/my-repository/api"
+	"github.com/jackc/pgx/v5"
 )
 
 func main() {
+	ctx := context.Background()
+
 	userHandler := initUser()
 	filmHandler := initFilm()
 
+	dbURL := "postgres://postgres:senha@localhost:5433/films_go"
+
+	conn, err := pgx.Connect(ctx, dbURL)
+	if err != nil {
+		slog.Error("Erro ao conectar no banco", "error", err.Error())
+		os.Exit(1)
+	}
+	defer conn.Close(ctx)
+
+	slog.Info("Conexão estabelcida com sucesso")
+
 	api.StartApp(userHandler, filmHandler)
+
 }
