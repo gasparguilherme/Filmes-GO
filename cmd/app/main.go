a67006a74b8c81ad4f1f9c8717c5e0d8cf0e3459
Guilherme Gaspar <guilhermegaspar474@gmail.com>
2026-02-13 13:30:01 -0300

    adiciona configuração de variáveis de ambiente com godotenv

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -7,12 +7,25 @@ import (
 
 	"github.com/gasparguilherme/my-repository/api"
 	"github.com/jackc/pgx/v5"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		slog.Error("Erro ao carregar .env", "error", err)
+		os.Exit(1)
+	}
+
 	ctx := context.Background()
 
-	dbURL := "postgres://postgres:senha@localhost:5433/films_go"
+	//pego o ambiente
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		slog.Error("DB_URL não encontrada no .env")
+		os.Exit(1)
+	}
 
 	conn, err := pgx.Connect(ctx, dbURL)
 	if err != nil {
--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 	github.com/golang-jwt/jwt/v5 v5.3.1 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	golang.org/x/crypto v0.37.0 // indirect
 	golang.org/x/text v0.24.0 // indirect
 )
